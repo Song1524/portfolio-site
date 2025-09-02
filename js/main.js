7f0b3a5468aff73cf75234f07e2970a71c21093b
zsdx1524 <zsdx1525@gmail.com>
2025-09-02 12:09:23 +0900

    0902

--- js/main.js
+++ js/main.js
@@ -63,3 +63,67 @@ imageModalBtnList.forEach(function (imageModalBtn) {
 imageCloseBtn.addEventListener('click', function () {
   imageModal.style.display = 'none';
 });
+
+// 모달 바깥 영역 클릭 시 닫기
+modal.addEventListener('click', function (e) { // 이벤트 발생 시 이벤트 객체가 전달됨
+  // console.log(e.target);  // 현재 이벤트가 발생한 대상(사용자가 실제 클릭한 가장 안쪽 요소)
+  // console.log(e.currentTarget); // 이벤트가 바인딩된 요소(모달)
+  
+  // if (e.target === modal) {  // 하드 코딩
+  if (e.target === e.currentTarget) {
+    modal.style.display = 'none';
+  }
+});
+imageModal.addEventListener('click', function (e) {
+  if (e.target === e.currentTarget) {
+    imageModal.style.display = 'none';
+  }
+});
+
+// ESC를 누르면 모달 닫기(키보드 이벤트)
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    modal.style.display = 'none';
+    imageModal.style.display = 'none';
+  }
+});
+
+// 현재 연도 표시
+// 날짜 정보를 가진 JS의 Date 객체를 활용
+console.log(new Date().getFullYear());
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear();
+
+// 페이지 최상단으로 이동
+const toTopEl = document.querySelector('#toTop');
+//Quiz: visual 섹션 애니메이션 넣고 빼기
+const visualSpanEls = document.querySelectorAll('.visual h1 span');
+
+// 페이지에 스크롤 이벤트 감지를 추가
+// 브라우저는 문서 전체의 스크롤을 window 기분으로 처리 
+// document에 붙이면 일부 브라우저에서는 동작 안 함
+// window: 브라우저 창 객체
+
+window.addEventListener('scroll', function () {
+  const goTop = document.querySelector('#toTop');
+
+  if (window.scrollY > 500) {
+    goTop.style.opacity = 1;
+    goTop.style.transform = 'translateX(0)';
+
+    // 애니메이션 빼기
+    visualSpanEls.forEach(function (visualSpan) {
+      visualSpan.classList.remove('animate-flash');
+    });
+
+  } else {
+    goTop.style.opacity = 0;
+    goTop.style.transform = 'translateX(100px)';
+
+    // 애니메이션 넣기
+    visualSpanEls.forEach(function (visualSpan) {
+      visualSpan.classList.add('animate-flash');
+    });
+  }
+});
+
